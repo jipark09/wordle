411c034fdcaa8738d7cf462b28bfd880276618ab
JEINI <jipark09@naver.com>
2025-08-06 21:46:48 +0900

    [wordle] css, Javascript 로직 부분 완성
    1. 5글자 단어 (존재하는 단어가 아니어도 됨)
    2. 6번의 시도 가능
    3. 존재하면 노란색, 위치도 맞으면 초록색으로 표시
    4. 게임 종료 판단
    5. 상단에 게임 시간 표시하기

---
+++ js/index.js
@@ -0,0 +1,115 @@
+const correct = "TABLE";
+let attempts = 0;
+let index = 0;
+const startTime = new Date();
+let timer;
+
+function appStart() {
+  // 화면 단 게임 종료
+  const displayGameEnd = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료되었습니다.";
+    div.classList.add("game-end-message");
+    // document.body.appendChild(div);
+    const mainEl = document.getElementById('main');
+    mainEl.appendChild(div);
+  };
+
+  const gameEnd = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameEnd();
+    clearInterval(timer);
+  };
+
+  // 백스페이스 눌렀을 떄의 동작
+  const handleBackspaceKey = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+
+      preBlock.innerText = "";
+    }
+
+    if (index !== 0) {
+      index--;
+    }
+  };
+
+  // 엔터키 눌렀을 때의 동작
+  const handleEnterKey = () => {
+    let correctCount = 0;
+    // 정답 확인
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const userText = block.innerText;
+      const correctText = correct[i];
+
+      if (userText === correctText) {
+        correctCount++;
+        block.style.backgroundColor = "#6AAA64";
+      } else if (correct.includes(userText)) {
+        block.style.backgroundColor = "#C9B458";
+      } else {
+        block.style.backgroundColor = "#787C7E";
+      }
+      block.style.color = "white";
+    }
+
+    // 게임 종료
+    if (correctCount === 5) {
+      gameEnd();
+    } else {
+      // 게임 종료 아니면, 다음 줄 넘김
+      attempts++;
+      // 다음 줄이 없으면 게임 종료
+      if (attempts === 6) {
+        gameEnd();
+        return;
+      }
+      index = 0;
+    }
+  };
+
+  // 키 다운했을 때의 동작
+  const handleKeydown = (e) => {
+    const key = e.key.toUpperCase();
+    const keyCode = e.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    if (e.key === "Backspace") {
+      handleBackspaceKey();
+    } else if (index === 5) {
+      if (e.key === "Enter") {
+        handleEnterKey();
+      }
+      return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index++;
+    }
+  };
+
+  // 타이머 기능
+  const startTimer = () => {
+    function setTime() {
+      const nowTime = new Date();
+      const progressTime = new Date(nowTime - startTime);
+      const minutes = progressTime.getMinutes().toString().padStart(2, "0");
+      const seconds = progressTime.getSeconds().toString().padStart(2, "0");
+      const timerDiv = document.querySelector(".timer");
+      timerDiv.innerText = `${minutes}:${seconds}`;
+    }
+
+    timer = setInterval(setTime, 1000); // 고유 id 반환: 타이머를 멈출 때 필요
+  };
+
+  startTimer();
+  window.addEventListener("keydown", handleKeydown); //handleKeydown > 암묵적으로 이벤트 전달(e)
+}
+
+appStart();
